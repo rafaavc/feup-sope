28d498322d05371b644cd8a284a094e1a2562a92
Rafael Cristino <rafavc2508@gmail.com>
2020-03-11 09:45:50 +0000

    [TP3] ex6 done

---
+++ TP3/ex6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+int main(void) {
+    pid_t pid;
+    int i, j, ret;
+    printf("I'm process %d. My parent is %d.\n", getpid(),getppid());
+    for (i=1; i<=3; i++) {
+        pid = fork();
+        if ( pid < 0) {
+            printf("fork error");
+            exit(1);
+        }
+        else if (pid == 0) {
+            printf("I'm process %d. My parent is %d. I'm going to work for 1 second ...\n", getpid(),getppid());
+            sleep(1); // simulando o trabalho do filho
+            printf("I'm process %d. My parent is %d. I finished my work\n", getpid(),getppid());
+            
+            exit(0); // important
+        } else {
+            // simulando o trabalho do pai
+            for (j=1; j<=10; j++) {
+                sleep(1);
+                printf("father working ...\n");
+                waitpid(pid, &ret, WNOHANG); // Doesn't block the program and prevents zombie processes
+                if (!WIFEXITED(ret)) {
+                    write(STDOUT_FILENO, "Child disappointed parent!\n", 27);
+                    exit(1);
+                }
+            }
+        }
+    }
+    exit(0);
+}
