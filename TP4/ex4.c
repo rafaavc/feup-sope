5da825be868e9d46d84e7f2ca9021a42a2b0f859
Rafael Cristino <rafavc2508@gmail.com>
2020-03-31 19:58:20 +0100

    [TP4] ex4 done, not sure I understood what it was trying to prove

--- TP4/ex4.c
+++ TP4/ex4.c
@@ -2,17 +2,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
+
+pid_t pid;
+int status;
+
+void sigchild_handler(int signo) {
+    pid = waitpid(-1, &status, WNOHANG);
+    printf("sigchildhandler\n");
+}
 
 int main(void) {
-    pid_t pid;
-    int i, n, status;
+    pid_t pid1;
+    int i, n;
+
+    struct sigaction sigchildAction;
+
+    sigchildAction.sa_handler = sigchild_handler;
+    sigemptyset(&sigchildAction.sa_mask);
+    sigchildAction.sa_flags = 0;
+
+    if (sigaction(SIGCHLD, &sigchildAction, NULL)!=0) {
+        printf("Error while setting sigchild handler.\n");
+    }   
+
 
     for (i=1; i<=3; i++) {
-        pid=fork();
-        if (pid == 0){
+        pid1=fork();
+        if (pid1 == 0){
             printf("CHILD no. %d (PID=%d) working ... \n",i,getpid());
             sleep(i*7); // child working ...
             printf("CHILD no. %d (PID=%d) exiting ... \n",i,getpid());
+            raise(SIGCHLD);
             exit(0);
         }
     }
@@ -23,7 +44,6 @@ int main(void) {
         printf("PARENT: end of task no. %d\n",i);
         printf("PARENT: waiting for child no. %d ...\n",i);
 
-        pid=wait(&status);
         if (pid != -1)
             printf("PARENT: child with PID=%d terminated with exit code %d\n",pid,WEXITSTATUS(status));
     }
