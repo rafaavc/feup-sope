52de30c9d9a47ba2dd94e84e1220ef3eb7b94523
Rafael Cristino <rafavc2508@gmail.com>
2020-03-31 20:19:12 +0100

    [TP4] ex5 done

--- TP4/ex4.c
+++ TP4/ex4.c
@@ -3,18 +3,18 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <errno.h>
 
-pid_t pid;
+pid_t pid = 0;
 int status;
 
 void sigchild_handler(int signo) {
     pid = waitpid(-1, &status, WNOHANG);
-    printf("sigchildhandler\n");
 }
 
 int main(void) {
     pid_t pid1;
-    int i, n;
+    int i;
 
     struct sigaction sigchildAction;
 
@@ -24,21 +24,33 @@ int main(void) {
 
     if (sigaction(SIGCHLD, &sigchildAction, NULL)!=0) {
         printf("Error while setting sigchild handler.\n");
+        exit(1);
     }   
 
 
-    for (i=1; i<=3; i++) {
+    for (i=1; i<=10; i++) {
         pid1=fork();
         if (pid1 == 0){
             printf("CHILD no. %d (PID=%d) working ... \n",i,getpid());
-            sleep(i*7); // child working ...
+            sleep(7+i); // child working ...
             printf("CHILD no. %d (PID=%d) exiting ... \n",i,getpid());
             raise(SIGCHLD);
             exit(0);
         }
     }
+    int count = 0;
+    while(count != 10) {
+        if (pid != 0) {
+            if (pid > 0) {
+                printf("Child with pid=%d just terminated with status code=%d.\n", pid, status);
+            } else if (pid == -1) {
+                printf("ERRNO: %d\n\n", errno);
+            }
+            pid = 0;
+        }
+    }
 
-    for (i=1 ;i<=4; i++ ) {
+    /*for (i=1 ;i<=4; i++ ) {
         printf("PARENT: working hard (task no. %d) ...\n",i);
         n=20; while((n=sleep(n))!=0);
         printf("PARENT: end of task no. %d\n",i);
@@ -46,6 +58,6 @@ int main(void) {
 
         if (pid != -1)
             printf("PARENT: child with PID=%d terminated with exit code %d\n",pid,WEXITSTATUS(status));
-    }
+    }*/
     exit(0);
 }
