1bd54dc7f974190e80b3e76bba094dcf4475d6aa
rafaavc <rafavc2508@gmail.com>
2020-04-27 20:15:16 +0100

    [TP6] ex2 done

---
+++ TP6/ex2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define STDERR 2
+#define NUMITER 1000
+
+int n = 100000;
+
+void * thrfunc(void * arg) {
+    int count = 0;
+    fprintf(stderr, "Starting thread %s\n", (char *) arg);
+    while (n > 0) {
+        n--;
+        count++;
+        write(STDERR,arg,1);
+    }
+    int * r = malloc(sizeof(int));
+    *r = count;
+    return (void *) r;
+}
+
+int main() {
+    pthread_t ta, tb;
+    void * c1, * c2;
+    pthread_create(&ta, NULL, thrfunc, "1");
+    pthread_create(&tb, NULL, thrfunc, "2");
+    pthread_join(ta, (void *) &c1);
+    pthread_join(tb, (void *) &c2);
+
+    printf("\nthread 1: %d, thread 2: %d\n", * (int*) c1, * (int*) c2);
+    return 0;
+}
